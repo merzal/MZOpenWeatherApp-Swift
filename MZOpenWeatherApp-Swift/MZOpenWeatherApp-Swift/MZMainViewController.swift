4fe6ef86a1bcaafe70922652b0ec150d33bd374c
Zalan Mergl <merzal@gmail.com>
2016-04-18 20:22:47 +0200

    Initial project commit

---
+++ MZOpenWeatherApp-Swift/MZOpenWeatherApp-Swift/MZMainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MZOpenWeatherApp-Swift
+//
+//  Created by Zalan Mergl on 14/12/15.
+//  Copyright © 2015 Zalan Mergl. All rights reserved.
+//
+
+import UIKit
+
+class MZMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
